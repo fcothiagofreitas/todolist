d991047ecd1b708f74cc287a1fc1b9e295de467b
Thiago Freitas <fcothiagofreitas@gmail.com>
2023-05-01 21:47:40 -0300

    ListItem v0

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,7 +1,7 @@
 import { Alert, Text, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 import { AntDesign } from '@expo/vector-icons';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Check from '../../assets/images/check.svg';
 import Checked from '../../assets/images/checked.svg';
 
@@ -10,36 +10,41 @@ type Props = {
   selecionado: boolean;
   onChange: any;
   textoItem: string;
+  onRemove: () => void;
+  onSelect: () => void;
 };
 
-export function ListItem({ textoItem, selecionado, onChange }: Props) {
+export function ListItem({ textoItem, onRemove, onSelect }: Props) {
   const [selecionar, setSelecionar] = useState(false);
 
   if (selecionar == true) {
     return (
       <>
-        <View
+        <TouchableOpacity
           style={styles.listitenSelecionado}
           onStartShouldSetResponder={() => setSelecionar(!selecionar)}
         >
           <Checked style={styles.checkIcon} />
           <Text style={styles.tituloSelecionado}>{textoItem}</Text>
-          <TouchableOpacity style={styles.deleteBtn}>
+          <TouchableOpacity style={styles.deleteBtn} onPress={onRemove}>
             <AntDesign name="delete" size={16} style={styles.delete} />
           </TouchableOpacity>
-        </View>
+        </TouchableOpacity>
       </>
     );
   } else {
     return (
       <>
-        <View style={styles.listiten} onStartShouldSetResponder={() => setSelecionar(!selecionar)}>
+        <TouchableOpacity
+          style={styles.listiten}
+          onStartShouldSetResponder={() => setSelecionar(!selecionar)}
+        >
           <Check style={styles.checkIcon} />
           <Text style={styles.titulo}>{textoItem}</Text>
-          <TouchableOpacity style={styles.deleteBtn}>
+          <TouchableOpacity style={styles.deleteBtn} onPress={onRemove}>
             <AntDesign name="delete" size={16} style={styles.delete} />
           </TouchableOpacity>
-        </View>
+        </TouchableOpacity>
       </>
     );
   }
